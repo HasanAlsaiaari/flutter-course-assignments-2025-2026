bfe8bede995e3048b344fed1c579b12a006eaea6
Hasan Alsaiaari <hasanalisaeed770@gmail.com>
2025-12-11 12:46:51 +0300

    TodoApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,149 +1,49 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(TodoApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TodoApp extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: TasbeehScreen(),
-    );
-  }
+  _TodoAppState createState() => _TodoAppState();
 }
 
-class TasbeehScreen extends StatefulWidget {
-  @override
-  _TasbeehScreenState createState() => _TasbeehScreenState();
-}
+class _TodoAppState extends State<TodoApp> {
+  final List<String> _todos = [];
 
-class _TasbeehScreenState extends State<TasbeehScreen> {
-  String subhantext = 'سبحان الله';
-  String hamdtext = 'الحمد لله';
-  String akbartext = 'الله أكبر';
-
-  int subhanCount = 0;
-  int hamdCount = 0;
-  int akbarCount = 0;
-
-  String displayedText = '';
-  String dispalyedCount = '0';
-
-  void incrementSubhan() {
-    setState(() {
-      subhanCount++;
-      displayedText = subhantext;
-      dispalyedCount = subhanCount.toString();
-    });
-  }
-
-  void incrementHamd() {
-    setState(() {
-      hamdCount++;
-      displayedText = hamdtext;
-      dispalyedCount = hamdCount.toString();
-    });
-  }
-
-  void incrementAkbar() {
-    setState(() {
-      akbarCount++;
-      displayedText = akbartext;
-      dispalyedCount = akbarCount.toString();
-    });
+  void _addTodoItem(String task) {
+    if (task.isNotEmpty) {
+      setState(() {
+        _todos.add(task);
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.purple.shade50,
-      appBar: AppBar(
-        title: Text(
-          'مسبحة إلكترونية',
-          style: TextStyle(fontFamily: 'Arial', fontWeight: FontWeight.bold),
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Simple To-Do App'),
         ),
-        centerTitle: true,
-        backgroundColor: Colors.purple.shade200,
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              displayedText,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+        body: Column(
+          children: <Widget>[
+            TextField(
+              onSubmitted: _addTodoItem,
+              decoration: InputDecoration(
+                labelText: 'Enter a new task',
+              ),
             ),
-
-            Text(
-              dispalyedCount,
-              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-
-            SizedBox(height: 50),
-
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: incrementSubhan,
-                      child: Text('سبحان الله'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.purple.shade700,
-                        shape: StadiumBorder(),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 30,
-                          vertical: 12,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 25, width: 25),
-
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: incrementHamd,
-                      child: Text('الحمد لله'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.purple.shade700,
-                        shape: StadiumBorder(),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 30,
-                          vertical: 12,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-
-                SizedBox(height: 25, width: 25),
-
-                Column(
-                  children: [
-                    ElevatedButton(
-                      onPressed: incrementAkbar,
-                      child: Text('الله أكبر'),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.white,
-                        foregroundColor: Colors.purple.shade700,
-                        shape: StadiumBorder(),
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 30,
-                          vertical: 12,
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            Expanded(
+              child: ListView.builder(
+                itemCount: _todos.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(_todos[index]),
+                  );
+                },
+              ),
             ),
           ],
         ),
